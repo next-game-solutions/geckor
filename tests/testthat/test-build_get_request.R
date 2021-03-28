9bbe9e29798d13afba14cd8be2052e5a7986ac4e
Sergey Mastitsky <mastitsky@gmail.com>
2021-03-28 16:14:58 +0100

    docs and tests for build_get_request

--- tests/testthat/test-build_get_request.R
+++ tests/testthat/test-build_get_request.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("build_get_request returns correct value", {
+  r <- build_get_request("https://api.coingecko.com",
+                         path = c("api", "v3", "ping"),
+                         query_parameters = NULL)
+  expect_identical(r, "https://api.coingecko.com/api/v3/ping")
 })
