ad48c787d2fc733d917a62b42546fe65e3c68a61
Sergey Mastitsky <mastitsky@gmail.com>
2021-03-28 16:54:10 +0100

    first few tests for validate_arguments

---
+++ tests/testthat/test-validate_arguments.R
@@ -0,0 +1,15 @@
+test_that("validate_arguments throws errors as expected", {
+
+  expect_error(validate_arguments(arg_max_attempts = "1L"))
+
+  expect_error(validate_arguments(arg_max_attempts = -1L))
+})
+
+
+test_that("validate_arguments returns nothing as expected", {
+  expect_null(
+    validate_arguments(
+      arg_max_attempts = 3L
+    )
+  )
+})
