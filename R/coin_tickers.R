97a1f45655bde48216ec32fb7f34b29a38cfff07
Sergey Mastitsky <mastitsky@gmail.com>
2021-06-21 21:04:08 +0100

    docs for coin_tickers

--- R/coin_tickers.R
+++ R/coin_tickers.R
@@ -1,3 +1,73 @@
+#' Exchange tickers
+#'
+#' Retrieves the most recent tickers for a coin
+#'
+#' @param coin_id (character): ID of the coin of interest. An up-to-date list of
+#'     supported coins and their IDs can be retrieved with the
+#'     [supported_coins()] function.
+#' @param exchange_id (character): ID of the exchange to retrive the data from.
+#'     An up-to-date list of supported exchanges and their IDs can be retrieved
+#'     with the [supported_exchanges()] function.
+#' @eval function_params(c("max_attempts", "api_note"))
+#'
+#' @return A tibble with the following columns:
+#'
+#' * `exchange_id` (character): exchange ID;
+#' * `exchange_name` (character): common name of the exchange;
+#' * `coin_id` (character): coin ID;
+#' * `name` (character): common name of the coin;
+#' * `base` (character): symbol of the base currency in the trading pair;
+#' * `target` (character): symbol of the target currency in the trading pair;
+#' * `trust_score` (character): trust score of this trading pair at this exchange
+#' (`"green"`, `"orange"`, `"red"`; see
+#' [this](https://blog.coingecko.com/trust-score/) and
+#' [this](https://blog.coingecko.com/trust-score-2/) articles on the CoinGecko
+#' website);
+#' * `last_price` (double): last price reported by this exchange for this
+#' trading pair;
+#' * `last_fetch_at` (POSIXct, UTC time zone): timestamp of when `last_price`
+#' was recorded;
+#' * `last_traded_at` (POSIXct, UTC time zone): timestamp of the most recent
+#' trade;
+#' * `bid_ask_spread_percentage` (double): percentage difference between
+#' the ask price (lowest price a seller is willing to sell) and the bid price
+#' (highest price a buyer is willing to buy; see
+#' [Investopedia](https://www.investopedia.com/terms/b/bid-askspread.asp) for
+#' details);
+#' `trading_volume_24` (double): trading volume (in `target` currency)
+#' recorded in the last 24 hours (as of `last_traded_at`);
+#' * `last_price_btc` (double): last price reported by this exchange for this
+#' `coin_id`, expressed in Bitcoin;
+#' * `last_price_eth` (double): last price reported by this exchange for this
+#' `coin_id`, expressed in Ethereum;
+#' * `last_price_usd` (double): last price reported by this exchange for this
+#' * `coin_id`, expressed in US dollars;
+#' * `trading_volume_24h_btc` (double): 24-hours trading volume expressed in
+#' Bitcoin (as of `last_traded_at`);
+#' * `trading_volume_24h_eth` (double): 24-hours trading volume expressed in
+#' Ethereum;
+#' * `trading_volume_24h_usd` (double): 24-hours trading volume expressed in
+#' US dollars;
+#' * `cost_to_move_up_2percent_usd` and `cost_to_move_down_2percent_usd`
+#' (double): 2% market depth (see
+#' [this](https://blog.coingecko.com/trust-score/) article on the CoinGecko
+#' website for details);
+#' * `is_anomaly` (logical): an indicator of whether the ticker's price is an
+#' outlier (see "Outlier detection" in
+#' [Methodology](https://www.coingecko.com/en/methodology) on the GoinGecko
+#' website);
+#' * `is_stale` (logical): an indicator of whether the ticker's price nos not
+#' been updated for a while (see "Outlier detection" in
+#' [Methodology](https://www.coingecko.com/en/methodology) on the GoinGecko
+#' website);
+#' * `trade_url` (character): URL to this trading pair's page.
+#'
+#' @export
+#'
+#' @examples
+#' r <- coin_tickers(coin_id = "cardano", exchange_id = "binance")
+#' print(r)
+#'
 coin_tickers <- function(coin_id,
                          exchange_id,
                          max_attempts = 3L) {
@@ -71,14 +141,14 @@ coin_tickers <- function(coin_id,
                                   format = "%Y-%m-%d %H:%M:%S"),
       bid_ask_spread_percentage = x$bid_ask_spread_percentage,
       trading_volume_24h = x$volume,
-      last_price_in_btc = x$converted_last$btc,
-      last_price_in_eth = x$converted_last$eth,
-      last_price_in_usd = x$converted_last$usd,
-      trading_volume_24h_in_btc = x$converted_volume$btc,
-      trading_volume_24h_in_eth = x$converted_volume$eth,
-      trading_volume_24h_in_usd = x$converted_volume$usd,
-      cost_to_move_up_2percent_in_usd = x$cost_to_move_up_usd,
-      cost_to_move_down_2percent_in_usd = x$cost_to_move_down_usd,
+      last_price_btc = x$converted_last$btc,
+      last_price_eth = x$converted_last$eth,
+      last_price_usd = x$converted_last$usd,
+      trading_volume_24h_btc = x$converted_volume$btc,
+      trading_volume_24h_eth = x$converted_volume$eth,
+      trading_volume_24h_usd = x$converted_volume$usd,
+      cost_to_move_up_2percent_usd = x$cost_to_move_up_usd,
+      cost_to_move_down_2percent_usd = x$cost_to_move_down_usd,
       is_anomaly = x$is_anomaly,
       is_stale = x$is_stale,
       trade_url = x$trade_url
