47419a2eadfd420b128340527bedd4bf0bfc39ea
Sergey Mastitsky <mastitsky@gmail.com>
2021-06-25 21:54:56 +0100

    tests for exchange_rate

--- R/exchange_rate.R
+++ R/exchange_rate.R
@@ -1,3 +1,34 @@
+#' Exchange rates
+#'
+#' Retrieves current exchange rate for a crypto- of fiat currency in Bitcoin
+#' @param currency (character or `NULL`): a vector with abbreviated names of the
+#'     currencies of interest. An up-to-date list of supported currencies (both
+#'     fiat and cryptocurrencies) can be retrieved with the [supported_currencies()]
+#'     function. If an unsupported `currency` is requested, the call will fail
+#'     with the respective error message. If `currency = NULL` (default), the
+#'     function will return exchange rates for all supported currencies.
+#'
+#' @eval function_params(c("max_attempts", "api_note"))
+#'
+#' @return A tibble with the following columns:
+#'
+#' * `timestamp` (POSIXct): date and time of the API request;
+#' * `currency` (character): abbreviated name of the currency;
+#' * `name` (character): common name of the currency;
+#' * `price_in_btc` (double): price in Bitcoin;
+#' * `type` (character): type of the currency (`"fiat"` or `"crypto"`).
+#'
+#' @export
+#'
+#' @examples
+#' # get exchange rates for all supported currencies
+#' r1 <- exchange_rate()
+#' print(r1)
+#'
+#' # get exchange rates for a set of currencies:
+#' r2 <- exchange_rate(currency = c("usd", "eur", "gbp"))
+#' print(r2)
+#'
 exchange_rate <- function(currency = NULL,
                           max_attempts = 3L) {
   validate_arguments(
@@ -11,6 +42,8 @@ exchange_rate <- function(currency = NULL,
     query_parameters = NULL
   )
 
+  ts <- Sys.time()
+
   r <- api_request(
     url = url,
     max_attempts = max_attempts
@@ -23,10 +56,13 @@ exchange_rate <- function(currency = NULL,
   result <- r %>%
     lapply(tibble::as_tibble) %>%
     dplyr::bind_rows() %>%
-    dplyr::select(-unit)
+    dplyr::select(-unit) %>%
+    dplyr::rename(price_in_btc = .data$value)
 
   return(
-    dplyr::bind_cols(id = names(r), result) %>%
-      dplyr::arrange(id)
+    dplyr::bind_cols(timestamp = ts,
+                     currency = names(r),
+                     result) %>%
+      dplyr::arrange(currency)
   )
 }
